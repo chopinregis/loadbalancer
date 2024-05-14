45aa48db7218bd65d6ddd757e845362309c59a43
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-13 20:25:40 -0400

    Update variable.tf

--- root/variable.tf
+++ root/variable.tf
@@ -1,1 +1,18 @@
+variable "subscription_id"{
+  type=string
+}
 
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+
+variable "myresourcegroupname" {
+  type = string
+  default = "mcit420zz5um"
+}
