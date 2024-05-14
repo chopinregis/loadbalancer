e6fc3362fc691ec5bad7da19afb2e84e8fc70952
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-13 20:29:02 -0400

    Update main.tf

--- root/main.tf
+++ root/main.tf
@@ -2,3 +2,32 @@ resource "azurerm_resource_group" "regisresourcegroup" {
   name     = var.myresourcegroupname
   location = "canadacentral"
 }
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-lb"
+  location = "West Europe"
+}
+
+module "mylb" {
+  source              = "Azure/loadbalancer/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  prefix              = "terraform-test"
+
+  remote_port = {
+    ssh = ["Tcp", "22"]
+  }
+
+  lb_port = {
+    http = ["80", "Tcp", "80"]
+  }
+
+  lb_probe = {
+    http = ["Tcp", "80", ""]
+  }
+
+}
