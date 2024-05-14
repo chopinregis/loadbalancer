32d8437dc35b79d50dad1f75fae896001008d13b
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-13 20:39:12 -0400

    Update main.tf

--- root/main.tf
+++ root/main.tf
@@ -5,25 +5,24 @@ resource "azurerm_resource_group" "regisresourcegroup" {
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-lb"
-  location = var.myresourcegrouplocation
+  name     = "LoadBalancerRG"
+  location = "West Europe"
 }
 
-module "mylb" {
-  source              = "Azure/loadbalancer/azurerm"
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  prefix              = "terraform-test"
+  allocation_method   = "Static"
+}
 
-  remote_port = {
-    ssh = ["Tcp", "22"]
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
   }
-
-  lb_port = {
-    http = ["80", "Tcp", "80"]
-  }
-
-  lb_probe = {
-    http = ["Tcp", "80", ""]
-  }
-
 }
